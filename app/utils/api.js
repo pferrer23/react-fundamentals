8b840c47033b513a85b014b5008ea7e83fc35791
Pablo Ferrer <dtecnologia@dreamersoft.com>
2017-06-01 18:57:53 -0500

    implement the battle route and the api file

---
+++ app/utils/api.js
@@ -0,0 +1,64 @@
+var axios = require('axios');
+
+var id = "YOUR_CLIENT_ID";
+var sec = "YOUR_SECRET_ID";
+var params = "?client_id=" + id + "&client_secret=" + sec;
+
+function getProfile (username)  {
+  return axios.get("https://api.github.com/users/" + username + params)
+  .then(user => {
+    return user.data;
+  });
+}
+function getRepos (username) {
+  return axios.get("https://api.github.com/users/" + username + "/repos" + params + "&per_page=100")
+  // .then(repos => {
+  //   return repos.data;
+  // });
+}
+function getStarsCount (repos)  {
+  return repos.data.reduce((count, repo) => {
+    return count + repo.stargazers_count;
+  }, 0);
+}
+function calculateScore (profile, repos)  {
+  var followers = profile.followers;
+  var totalStars = getStarsCount(repos);
+  return (followers * 3) + totalStars;
+}
+function handleError (error) {
+  console.warn(error);
+  return null;
+}
+function getUserData (player) {
+  return axios.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then((data)=> {
+    var profile = data[0];
+    var repos = data[1];
+    return {
+      profile: profile,
+      score: calculateScore(profile, repos)
+    };
+  });
+}
+function sortPlayers (players) {
+  return players.sort((a,b) => {
+    return b.score - a.score;
+  });
+}
+module.exports = {
+  battle: players => {
+    return axios.all(players.map(getUserData))
+    .then(sortPlayers)
+    .catch(handleError);
+  },
+  fetchPopularRepos: function (language) {
+    var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories');
+    return axios.get(encodedURI)
+    .then(function (response) {
+      return response.data.items;
+    });
+  }
+}
